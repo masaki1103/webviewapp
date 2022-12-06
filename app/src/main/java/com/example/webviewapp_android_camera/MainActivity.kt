866ddba26ef4d50d9b8793df773d743f55945e2f
m-shinmachi <masaki.shinmachi@lassic.co.jp>
2022-12-06 16:08:56 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/webviewapp_android_camera/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.webviewapp_android_camera
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import java.util.jar.Pack200
+
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import android.webkit.PermissionRequest
+//import android.webkit.WebChromeClient
+//import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            println("permission granted")
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+        }
+
+        val WebView: WebView = findViewById(R.id.webview)
+        WebView.settings.javaScriptEnabled = true
+        WebView.settings.domStorageEnabled = true
+        WebView.setWebChromeClient(object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest) {
+                request.grant(request.resources)
+            }
+        })
+        WebView.loadUrl("https://aadi-video.app.100ms.live/preview/stj-lci-aah/")
+     }
+}
