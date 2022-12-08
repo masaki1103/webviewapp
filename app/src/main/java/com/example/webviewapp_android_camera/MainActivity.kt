b778206bb2621020fb389b2e1f6c16fccfaa0290
masaki1103 <tarochan1103@gmail.com>
2022-12-08 14:04:40 +0900

    更新

--- app/src/main/java/com/example/webviewapp_android_camera/MainActivity.kt
+++ app/src/main/java/com/example/webviewapp_android_camera/MainActivity.kt
@@ -5,39 +5,71 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
-import java.util.jar.Pack200
-
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.webkit.PermissionRequest
-//import android.webkit.WebChromeClient
-//import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            println("permission granted")
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                println("permission granted")
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+            }
         }
 
-        val WebView: WebView = findViewById(R.id.webview)
-        WebView.settings.javaScriptEnabled = true
-        WebView.settings.domStorageEnabled = true
-        WebView.setWebChromeClient(object : WebChromeClient() {
-            override fun onPermissionRequest(request: PermissionRequest) {
-                request.grant(request.resources)
+        val webView: WebView = findViewById(R.id.webview)
+        webView.apply {
+            settings.apply {
+                javaScriptEnabled = true
+                mediaPlaybackRequiresUserGesture = false
+                userAgentString = userAgentString + " qo.kenkoappli"
             }
-        })
-        WebView.loadUrl("https://aadi-video.app.100ms.live/preview/stj-lci-aah/")
+            webChromeClient = object : WebChromeClient() {
+                @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+                override fun onPermissionRequest(request: PermissionRequest?) {
+                    if (request?.resources!!.contains(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
+                        request.grant(request.resources)
+                    }
+                }
+            }
+            webViewClient = WebViewClient()
+        }
+//        webView.loadUrl("https://masaki1103.github.io/camera_demo/")
+//        webView.loadUrl("https://masaki1103.github.io/qrcode_demo/")
+        webView.loadUrl("https://masaki1103.github.io/capture_demo/")
      }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    println("permission granted")
+                } else {
+/*                   Handler.post(Runnable {
+                        RuntimePermissionUtils().showAlertDialog(
+                            supportFragmentManager,
+                            "オーディオの録音"
+                        )
+                    })
+*/
+                }
+                return
+            }
+        }
+    }
 }
